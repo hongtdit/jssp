2e93d172b2ebafdb42442ac57fb0123a29441b25
hongtdit <trandinhhong1997@gmail.com>
2022-11-09 00:06:13 +0700

    m

---
+++ script.js
@@ -0,0 +1,16 @@
+var name = 'Nguyen Huu Hung'
+// function Sum(){
+//     let h1 = document.getElementsByTagName('h1')[0].innerHTML;
+//     let result =Number(h1);
+//     document.getElementsByTagName('h1')[0].innerHTML=`${result+1}`;
+// }
+// function Tru(){
+//     let h1 = document.getElementsByTagName('h1')[0].innerHTML;
+//     let result =Number(h1);
+//     document.getElementsByTagName('h1')[0].innerHTML=`${result-1}`;
+//     console.log(document.getElementsByTagName('h1')[0].innerHTML)
+// }
+function Sum(){
+    const textColor = document.querySelector('.text');
+    textColor.style.color="red";
+}
